531d2d90c64022e18749ce486864142b2fc43792
sree858585 <srikanthgara116@gmail.com>
2026-07-08 11:43:52 -0400

    Add files via upload

---
+++ Models/SmtpSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace HIVTraining_Vue.Server.Models
+{
+    public class SmtpSettings
+    {
+        public bool IsDevelopment { get; set; }
+        public SmtpAccountSettings DevSettings { get; set; } = new();
+        public SmtpAccountSettings ProdSettings { get; set; } = new();
+    }
+
+    public class SmtpAccountSettings
+    {
+        public string Host { get; set; } = "";
+        public int Port { get; set; }
+        public bool EnableSSL { get; set; }
+        public string UserName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string FromName { get; set; } = "HIV Training";
+    }
+}
