a17c99368eb0dfd015164961dd8e766cde6aa654
sree858585 <srikanthgara116@gmail.com>
2026-07-08 11:28:06 -0400

    Add files via upload

---
+++ Services/EmailService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Mail;
+using HIVTraining_Vue.Server.Models;
+using Microsoft.Extensions.Options;
+
+namespace HIVTraining_Vue.Server.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
+        {
+            var settings = _smtpSettings.IsDevelopment
+                ? _smtpSettings.DevSettings
+                : _smtpSettings.ProdSettings;
+
+            using var smtpClient = new SmtpClient(settings.Host)
+            {
+                Port = settings.Port,
+                EnableSsl = settings.EnableSSL,
+                Credentials = new NetworkCredential(
+                    settings.UserName,
+                    settings.Password
+                )
+            };
+
+            using var mailMessage = new MailMessage
+            {
+                From = new MailAddress(settings.UserName, settings.FromName),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace HIVTraining_Vue.Server.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string htmlMessage);
+    }
+}
